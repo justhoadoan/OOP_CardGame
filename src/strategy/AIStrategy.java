cb7eb36ee20a8b4166653ac931baa971fc51d503
justhoadoan <hoatrongdoan1710@gmail.com>
2025-03-21 12:34:02 +0700

    update

--- src/strategy/AIStrategy.java
+++ src/strategy/AIStrategy.java
@@ -1,7 +1,9 @@
 package strategy;
 
 import gameaction.GameAction;
+import games.BlackJackGame;
 import games.Game;
+import games.GameType;
 import games.PokerGame;
 
 import java.util.List;
@@ -13,13 +15,28 @@ public class AIStrategy {
     public AIStrategy() {
         this.random = new Random();
     }
+    public GameAction decideAction(Game game, List<GameAction> availableActions) {
+        if (availableActions == null || availableActions.isEmpty()) {
+            throw new IllegalStateException("No available actions for AI");
+        }
+
+        if (game.getGameType() == GameType.POKER) {
+            return decidePokerAction((PokerGame) game, availableActions);
+        } else if (game.getGameType() == GameType.BLACKJACK) {
+            return decideBlackjackAction((BlackJackGame)game, availableActions);
+        } else {
+
+            throw new UnsupportedOperationException("Game type not supported: " + game.getGameType());
+        }
+    }
     //Tu's part
     public GameAction decidePokerAction(PokerGame pokerGame, List<GameAction> availableActions) {
+
         // AI logic here
         // For now, just return the first available action
         return availableActions.get(0);
     }
-    public GameAction decideBlackjackAction(List<GameAction> availableActions) {
+    public GameAction decideBlackjackAction(BlackJackGame blackJackGame, List<GameAction> availableActions) {
         // AI logic here
         // For now, just return the first available action
         return availableActions.get(0);
