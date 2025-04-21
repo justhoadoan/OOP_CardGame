6a8e976d8ceef890c07bca022a18268f5bf6fcee
Sepp1st <130156093+Sepp1st@users.noreply.github.com>
2025-04-21 21:54:01 +0700

    update poker

--- src/gui/PokerGame.java
+++ src/gui/PokerGame.java
@@ -5,8 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -18,28 +17,42 @@ public class PokerGame {
     @FXML private ImageView communityCard3;
     @FXML private ImageView communityCard4;
     @FXML private ImageView communityCard5;
-    @FXML private ImageView playerCard1;
-    @FXML private ImageView playerCard2;
+    @FXML private Pane player1CardArea;
+    @FXML private Pane player2CardArea;
+    @FXML private Pane player3CardArea;
+    @FXML private Pane player4CardArea;
+    @FXML private ImageView player1Card1;
+    @FXML private ImageView player1Card2;
+    @FXML private ImageView player2Card1;
+    @FXML private ImageView player2Card2;
+    @FXML private ImageView player3Card1;
+    @FXML private ImageView player3Card2;
+    @FXML private ImageView player4Card1;
+    @FXML private ImageView player4Card2;
     //@FXML private HBox communityHBox;
     @FXML private TextField raiseField;
     @FXML private Slider raiseSlider;
-    @FXML private StackPane opponentDeck;
-    //@FXML private StackPane playerDeck;
-    @FXML private Label player;
-    @FXML private Label playerMoney;
-    @FXML private Label opponentName;
-    @FXML private Label opponentMoney;
+    @FXML private Label player1Name;
+    @FXML private Label player1Money;
+    @FXML private Label player2Name;
+    @FXML private Label player2Money;
+    @FXML private Label player3Name;
+    @FXML private Label player3Money;
+    @FXML private Label player4Name;
+    @FXML private Label player4Money;
     @FXML private Text potMoney;
 
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
         //int amount = 500;
         //potMoney.setText(String.format("$%d", amount));
-        potMoney.setText("$500");
-        opponentName.setText("Opponent1");
-        opponentMoney.setText("$1000");
-        player.setText("Player");
-        playerMoney.setText("$1000");
+        potMoney.setText("$50000");
+        player2Name.setText("Player 2");
+        player2Money.setText("$1000");
+        player1Name.setText("Player 1");
+        player1Money.setText("$1000");
+        player3CardArea.setVisible(false);
+        player4CardArea.setVisible(false);
 
         //raiseSlider.setMin(0);
         //raiseSlider.setMax(1000);
